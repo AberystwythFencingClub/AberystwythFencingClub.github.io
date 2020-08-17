8207ca6d8988ea2d0e9eae10750a3d0147b40de7
Jason Cox <jasoninaber@gmail.com>
2020-08-17 07:48:43 +0100

    Updates

---
+++ precache-manifest.6c69037519ea5aa6242fab4ff5888bec.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8048217dc65f5e91d6009fd7a775cc9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55f85772b406a0d8b25a",
+    "url": "/static/css/2.3a395901.chunk.css"
+  },
+  {
+    "revision": "aa832b3da8e16a376cbd",
+    "url": "/static/css/main.00770f9f.chunk.css"
+  },
+  {
+    "revision": "55f85772b406a0d8b25a",
+    "url": "/static/js/2.a012f790.chunk.js"
+  },
+  {
+    "revision": "ab40d9aa0e6fc6976e4503799ee3716e",
+    "url": "/static/js/2.a012f790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa832b3da8e16a376cbd",
+    "url": "/static/js/main.b12e21a9.chunk.js"
+  },
+  {
+    "revision": "7632c6acda2e3c494e67",
+    "url": "/static/js/runtime-main.cbbb42e2.js"
+  },
+  {
+    "revision": "1bd6fb7010dc39b737a54fbe5cd51f05",
+    "url": "/static/media/Club.1bd6fb70.jpg"
+  },
+  {
+    "revision": "2185086577c099da9096735af007c60a",
+    "url": "/static/media/Sport-Wales-bilingual-Colour-logo.21850865.png"
+  },
+  {
+    "revision": "348807c7ecb7270d15fccff16ffe3fe4",
+    "url": "/static/media/arrow-right-icon.348807c7.svg"
+  },
+  {
+    "revision": "daa6dd44d99ff512fa3fdfd824cf442d",
+    "url": "/static/media/celebration-icon.daa6dd44.svg"
+  },
+  {
+    "revision": "09cc57a3737648e3052af81e0c83e65c",
+    "url": "/static/media/chef-icon.09cc57a3.svg"
+  },
+  {
+    "revision": "c66b213d1dfff391c61146727032f068",
+    "url": "/static/media/chevron-down.c66b213d.svg"
+  },
+  {
+    "revision": "7c1a6f97bf87de69a244342e6c17389c",
+    "url": "/static/media/customize-icon.7c1a6f97.svg"
+  },
+  {
+    "revision": "3ac89e2d233af035e25ca8334d179934",
+    "url": "/static/media/design-illustration.3ac89e2d.svg"
+  },
+  {
+    "revision": "1c251c8ff2222207650b4f24a2159cbb",
+    "url": "/static/media/dot-pattern.1c251c8f.svg"
+  },
+  {
+    "revision": "5e030c93ee64514b9fcdc887289ce8a9",
+    "url": "/static/media/facebook-icon.5e030c93.svg"
+  },
+  {
+    "revision": "ca394ec8a4754ab9db293dcac626bdcb",
+    "url": "/static/media/menu.ca394ec8.svg"
+  },
+  {
+    "revision": "36f0e6ee645c508fdc115426b87baba6",
+    "url": "/static/media/play-circle.36f0e6ee.svg"
+  },
+  {
+    "revision": "297d50a2cd8b864f0c1f357e5108d87a",
+    "url": "/static/media/shield-icon.297d50a2.svg"
+  },
+  {
+    "revision": "6c8f89cc4172b2cb49caeeef863d9692",
+    "url": "/static/media/shop-icon.6c8f89cc.svg"
+  },
+  {
+    "revision": "5d0ff9bc5818d6498df9d90437127f90",
+    "url": "/static/media/star-icon.5d0ff9bc.svg"
+  },
+  {
+    "revision": "a200bad45b923b0b206917b16755f83f",
+    "url": "/static/media/stats-illustration.a200bad4.svg"
+  },
+  {
+    "revision": "135d403ccd52c50bf2e773a8a68cea57",
+    "url": "/static/media/support-icon.135d403c.svg"
+  },
+  {
+    "revision": "ec52da39f670db5b4b3c32f54914a919",
+    "url": "/static/media/svg-decorator-blob-1.ec52da39.svg"
+  },
+  {
+    "revision": "159be2476eedd8b63bb0c7e0a58d0e47",
+    "url": "/static/media/svg-decorator-blob-3.159be247.svg"
+  },
+  {
+    "revision": "50a3868db49539d53742411308690eba",
+    "url": "/static/media/svg-decorator-blob-5.50a3868d.svg"
+  },
+  {
+    "revision": "c86fb21a2396dc9a8149429f1cd451cf",
+    "url": "/static/media/svg-decorator-blob-7.c86fb21a.svg"
+  },
+  {
+    "revision": "5ae3fe7a0d10e5c14ec0575db276dcda",
+    "url": "/static/media/team-illustration-2.5ae3fe7a.svg"
+  },
+  {
+    "revision": "7189052ca672d113f057d9a4f3cb5efe",
+    "url": "/static/media/twitter-icon.7189052c.svg"
+  },
+  {
+    "revision": "b0200bf7cbbb26c32989e7a0e4d19eef",
+    "url": "/static/media/x.b0200bf7.svg"
+  },
+  {
+    "revision": "45e500e6c6c6577b063b28adc39fb422",
+    "url": "/static/media/youtube-icon.45e500e6.svg"
+  }
+]);
