ead0aa30dde750a51aa65cead00784312e6ca973
Jason Cox <jasoninaber@gmail.com>
2020-12-12 23:24:14 +0000

    Updates

--- precache-manifest.c6f229c6a2e35079ef97522c8231ac0b.js
+++ precache-manifest.c6f229c6a2e35079ef97522c8231ac0b.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0ee4765916d7c5de4655d821726bb700",
+    "revision": "19574d674814dae6ec2d412340894684",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.bb5dc4fb.chunk.css"
   },
   {
-    "revision": "c56959b5dd6e448ea26b",
+    "revision": "0d5a5df9e65ad4e1a3ef",
     "url": "/static/css/main.be9b2218.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.c52b2ed8.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c56959b5dd6e448ea26b",
-    "url": "/static/js/main.78be1723.chunk.js"
+    "revision": "0d5a5df9e65ad4e1a3ef",
+    "url": "/static/js/main.70ac75f5.chunk.js"
   },
   {
     "revision": "7632c6acda2e3c494e67",
